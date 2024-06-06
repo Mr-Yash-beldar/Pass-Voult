2f4f7ad575b8fc7331c6e5b102add184bf5d0b07
Yashodip Beldar <yashodip2003beldar@gmail.com>
2024-06-06 14:12:08 +0530

    web route file

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\VerifyUserController;
+use App\Http\Controllers\EmailController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+Route::get('email/send/{id}', [EmailController::class, 'sendEmailforOTP'])->name('sendEmailforOTP');
+Route::get('email/verify', [VerifyUserController::class, 'verifyUser'])->name('verifyUser');
+
+
+
+
+
